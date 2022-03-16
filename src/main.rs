461cb4dadfa3376a6a86687b383232175502a35f
Bradley Horlander <78397249+BradleySH@users.noreply.github.com>
2022-03-15 22:02:55 -0500

    added in tokio and cargo expand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}", &name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let body = async move {
+        HttpServer::new(|| {
+            App::new()
+                .route("/", web::get().to(greet))
+                .route("/name", web::get().to(greet))
+        })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+    };
+
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("Failed building the Runtimes")
+        .block_on(body)
 }
